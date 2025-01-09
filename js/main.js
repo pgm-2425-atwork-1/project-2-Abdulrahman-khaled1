05d17f9f42ee97ffefd2d991773b6c120b7121f7
Abdulrahman-khaled1 <abdulrahman.khaled@student.arteveldehs.be>
2025-01-09 23:29:24 +0100

    update headers and and dropdown menu

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-const menuIcon = document.getElementById('menu-icon');
-const navLinks = document.getElementById('nav-links')
+const menuIcon = document.getElementById("menu-icon");
+const navLinks = document.getElementById("nav-links");
 
-menuIcon.addEventListener('click', ()=>{
-    navLinks.classList.toggle('active')
+menuIcon.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
 });
 
 // Sluit de navigatielijst als er buiten het menu-icoon of de lijst wordt geklikt
-document.addEventListener('click', (event) => {
-    if (!menuIcon.contains(event.target) && !navLinks.contains(event.target)) {
-        navLinks.classList.remove('active');
-    }
+document.addEventListener("click", (event) => {
+  if (!menuIcon.contains(event.target) && !navLinks.contains(event.target)) {
+    navLinks.classList.remove("active");
+  }
 });
