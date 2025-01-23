8a03d1bd6797682bf9227ff9c9be36028adf179a
Abdulrahman-khaled1 <abdulrahman.khaled@student.arteveldehs.be>
2025-01-23 21:36:12 +0100

    Refactored dark mode: improved with CSS variables, simplified JavaScript, used filter to change icon colors, and updated HTML for better maintainability and performance.

--- static/js/dark-mode.js
+++ static/js/dark-mode.js
@@ -1,29 +1,24 @@
 const html = document.documentElement;
-  const toggleMoon = document.getElementById('toggle-mode');
-  const toggleSun = document.getElementById('toggle-mode-sun');
+const toggleMoon = document.getElementById("toggle-mode");
+const toggleSun = document.getElementById("toggle-mode-sun");
 
-  toggleMoon.addEventListener('click', () => {
-    html.classList.toggle('dark-mode')
-    if (html.classList.contains('dark-mode')) {
-      toggleSun.style.display='block'
-      toggleMoon.style.display='none'
-    }
+//// om de status van dark mode in localStorage bij het laden van de pagina te controleren
+if (localStorage.getItem("dark-mode") === "enabled") {
+  //Als dark mode is ingeschakeld, voeg de klasse "dark-mode" toe aan de HTML-tag
+  html.classList.add("dark-mode");
+} else {
+  // Als het niet is ingeschakeld, verwijder het"
+  html.classList.remove("dark-mode");
+}
 
-    else{
-       toggleSun.style.display='none'
-      toggleMoon.style.display='block'
-    }
-    })
-  
-    toggleSun.addEventListener('click', ()=>{
-      html.classList.toggle('dark-mode')
-      if (html.classList.contains('dark-mode')) {
-        toggleSun.style.display='block'
-        toggleMoon.style.display='none'
-      }
-  
-      else{
-         toggleSun.style.display='none'
-        toggleMoon.style.display='block'
-      }
-      })
+// Functie om te schakelen tussen light mode en dark mode
+function togglleMode() {
+  html.classList.toggle("dark-mode");
+  if (html.classList.contains("dark-mode")) {
+    localStorage.setItem("dark-mode", "enabled");
+  } else {
+    localStorage.setItem("dark-mode", "disabled");
+  }
+}
+toggleMoon.addEventListener("click", togglleMode);
+toggleSun.addEventListener("click", togglleMode);
