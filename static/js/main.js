58c62c8d4de0c759ab9746c6e4d25356c489d6ba
Abdulrahman-khaled1 <abdulrahman.khaled@student.arteveldehs.be>
2025-01-24 00:14:18 +0100

    Implemented toggle functionality to switch between burger and close (X) icons on menu open/close.

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,28 @@
 const menuIcon = document.getElementById("menu-icon");
 const navLinks = document.getElementById("nav-links");
+const closeIcon = document.getElementById("close-icon");
 
 menuIcon.addEventListener("click", () => {
   navLinks.classList.toggle("active");
+  if (navLinks.classList.contains("active")) {
+    menuIcon.classList.add('hidden')
+    closeIcon.classList.remove('hidden')
+  }
+  else{
+    menuIcon.classList.remove('hidden')
+    closeIcon.classList.add('hidden')
+  }
+  
 });
+
+
 
 // Sluit de navigatielijst als er buiten het menu-icoon of de lijst wordt geklikt
 document.addEventListener("click", (event) => {
   if (!menuIcon.contains(event.target) && !navLinks.contains(event.target)) {
     navLinks.classList.remove("active");
+   menuIcon.classList.remove("hidden");  
+   closeIcon.classList.add("hidden");   
   }
 });
 
